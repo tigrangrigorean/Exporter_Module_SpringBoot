0b5da22bd2ad54c2d4cb0ef697010c1e8a05f208
= <tigran_grigoryan05@inbox.ru>
2023-05-24 11:15:11 +0400

    ExporterApp

---
+++ src/test/java/com/exporter/ExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
